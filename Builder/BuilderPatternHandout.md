588ae479870532cb539dcbacf6055ff77cc03831
Vivekanand P V <vivekanandpv@outlook.com>
2021-12-13 23:09:32 +0530

    builder pattern

---
+++ Builder/BuilderPatternHandout.md
@@ -0,0 +1,21 @@
+﻿# Builder Pattern
+
+
+## Motivation
+While creating complex objects (objects that have other objects composed within), the creation process becomes a complexity unto itself. We now have to deal with two related but different concerns: representation and creation. Representation means the state of the object. From a client perspective we wish to relegate this construction process so that both concerns are taken care of in a flexible way. 
+***So, how can a unified construction process create objects with different representations?*** -- This is the problem we solve.
+
+## Definition
+Separate the construction of a complex object from its representation so that the same construction process can create different representations..
+
+## Understanding
+*	State and composition structure of the object is called *representation*
+
+## Observations
+1.	Create an interface for Builder that deals with discrete steps of object representation
+2.	Create a Director which delegates the construction and representation to a Builder instance
+3.	Client uses Director for construction of the object and gets the final object by the concrete Builder instance
+
+## Remarks
+1.	This is the classic pattern as described in GoF book. There are other variants of Builder pattern, in which the Director and Builder are merged.
+2.	In some variations the final object may be polymorphic, though this approach is debatable
---
+++ Builder/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace Builder {
+    class Program {
+        static void Main(string[] args) {
+            //  Section 7
+            //  We need the reference to this concrete builder
+            var awsBuilder = new AWSServerBuilder();
+
+            //  Section 8
+            //  Client has to coordinate the dependency for the Director here
+            ServerDirector director = new ServerDirector(awsBuilder);
+
+            //  Section 9
+            //  Director kicks off the representation customization
+            director.Construct();
+
+            //  Section 10
+            //  The final object is still in the concrete builder
+            Server server = awsBuilder.GetServer();
+            Console.WriteLine(server);
+        }
+    }
+
+    //  Section: 1
+    //  Constituent classes for the final object
+    class CPU {
+        public string Description { get; set; }
+    }
+
+    class RAM {
+        public string Description { get; set; }
+    }
+
+    class NIC {
+        public string Description { get; set; }
+    }
+
+    class SSD {
+        public string Description { get; set; }
+    }
+
+    //  Section 2
+    //  Final complex object
+    class Server {
+        //  This is the representation
+        public CPU CPU { get; set; }
+        public RAM RAM { get; set; }
+        public NIC NIC { get; set; }
+        public SSD SSD { get; set; }
+
+        public override string ToString() {
+            return $"Server: \n\tCPU: {CPU.Description}\n\tRAM: {RAM.Description}\n\tNIC: {NIC.Description}\n\tSSD: {SSD.Description}";
+        }
+    }
+
+    //  Section 3
+    //  Builder interface exposes the API for customizing representation
+    interface IServerBuilder {
+        void AddCPU(string description);
+        void AddRAM(string description);
+        void AddNIC(string description);
+        void AddSSD(string description);
+    }
+
+    //  Section 4
+    //  Concrete builder
+    class AWSServerBuilder : IServerBuilder {
+        //  Creation
+        private readonly Server _server = new Server();
+
+        //  Representation
+        public void AddCPU(string description) {
+            _server.CPU = new CPU { Description = description };
+        }
+
+        public void AddRAM(string description) {
+            _server.RAM = new RAM { Description = description };
+        }
+
+        public void AddNIC(string description) {
+            _server.NIC = new NIC { Description = description };
+        }
+
+        public void AddSSD(string description) {
+            _server.SSD = new SSD { Description = description };
+        }
+
+        //  Conventionally: GetResult()
+        public Server GetServer() {
+            return _server;
+        }
+    }
+
+    //  Section 5
+    //  Director deals with customizing the representation
+    class ServerDirector {
+        private readonly IServerBuilder _builder;
+
+        public ServerDirector(IServerBuilder builder) {
+            _builder = builder;
+        }
+
+        //  Section 6
+        //  Customizing the representation
+        //  Consider abstracting Director for polymorphic Construct
+        public void Construct() {
+            _builder.AddCPU("Intel Core i5 @ 2GHz");
+            _builder.AddRAM("8GB DDR4 @ 2333MHz");
+            _builder.AddNIC("Broadcom Gigabit NIC");
+            _builder.AddSSD("250GB Samsung EVO SSD");
+        }
+    }
+}
